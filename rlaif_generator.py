b54075c34ef88d9550e37fdf709e78e5a68787c4
John David Pressman <jd@jdpressman.com>
2023-07-26 14:54:40 -0700

    Add multi-gpu RLAIF tuning

--- rlaif_generator.py
+++ rlaif_generator.py
@@ -15,6 +15,7 @@ import sys
 
 os.environ["BITSANDBYTES_NOWELCOME"] = "1"
 
+import accelerate
 import dice_mc.torch as dice
 import peft
 import torch
@@ -188,7 +189,7 @@ def main():
         __doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("--resume", type=str, default=None, help="Path to lora to resume from")
-    parser.add_argument("--batch-size", type=int, default=4, help="the batch size")
+    parser.add_argument("--batch-size", type=int, default=1, help="the batch size")
     parser.add_argument("--constitution", type=Path, required=True, help="the constitution to use")
     parser.add_argument(
         "--grad-accum-steps", type=int, default=4, help="the number of gradient accumulation steps"
@@ -202,12 +203,21 @@ def main():
     )
     args = parser.parse_args()
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    accelerator = accelerate.Accelerator(gradient_accumulation_steps=args.grad_accum_steps)
+    device = accelerator.device
+    print0 = accelerator.on_local_main_process(print)
 
     prompts = parse_prompts(args.prompts.read_text())
     cons = parse_constitution(args.constitution.read_text())
     principle_weights = [float(principle["weight"]) for principle in cons["principles"]]
     principle_weights = torch.tensor(principle_weights, device=device)
+    principle_signs = []
+    for principle in cons["principles"]:
+        answer = principle["answer"].lower()
+        if answer not in {"yes", "no"}:
+            raise ValueError("desired answer must be yes or no")
+        principle_signs.append(1 if answer == "yes" else -1)
+    principle_signs = torch.tensor(principle_signs, device=device)
 
     model_name = "openlm-research/open_llama_7b"
     evaluator_adapter_name = "RiversHaveWings/minihf_evaluator_openllama_7b"
@@ -225,7 +235,7 @@ def main():
     )
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        device_map="auto",
+        device_map="auto" if accelerator.num_processes == 1 else {"": device},
         quantization_config=bnb_config,
         torch_dtype=torch.float16,
         trust_remote_code=True,
@@ -247,7 +257,7 @@ def main():
             "mlp.gate_proj",
             "mlp.up_proj",
             "mlp.down_proj",
-            "lm_head",
+            # "lm_head",
         ],
     )
     if args.resume:
@@ -257,122 +267,139 @@ def main():
         model.train()
     # model.gradient_checkpointing_enable()
     # model.enable_input_require_grads()
-    model.print_trainable_parameters()
+    if accelerator.is_local_main_process:
+        model.print_trainable_parameters()
 
     inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(device)
     input_len = inputs.input_ids.shape[1]
 
     opt = optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.98))
-    scaler = torch.cuda.amp.GradScaler()
     sched = optim.lr_scheduler.LambdaLR(opt, constant_schedule(1.0))
     kl_sched = constant_schedule(args.kl_weight)
+
+    model, opt, sched = accelerator.prepare(model, opt, sched)
 
     baseline = dice.EMABaseline(decay=0.98).to(device)
     baseline_kl = dice.EMABaseline(decay=0.98).to(device)
 
-    for i in tqdm(endless_range()):
+    accelerator.wait_for_everyone()
+
+    for i in tqdm(endless_range(), disable=not accelerator.is_local_main_process):
         if i % 20 == 0:
-            with set_adapter(model, "default"):
-                outputs = model.generate(
-                    inputs.input_ids[:4],
-                    attention_mask=inputs.attention_mask[:4],
+            with set_adapter(accelerator.unwrap_model(model), "default"):
+                demo_examples = min(4, len(prompts))
+                demo_bs = math.ceil(demo_examples / accelerator.num_processes)
+                start_idx = accelerator.local_process_index * demo_bs
+                end_idx = start_idx + demo_bs
+                outputs = accelerator.unwrap_model(model).generate(
+                    inputs.input_ids[start_idx:end_idx],
+                    attention_mask=inputs.attention_mask[start_idx:end_idx],
                     do_sample=True,
                     min_new_tokens=args.length,
                     max_new_tokens=args.length,
                     pad_token_id=tokenizer.eos_token_id,
                     top_k=0,
                 )
+            outputs = accelerator.gather(outputs)[:demo_examples]
             text = [tokenizer.decode(t, skip_special_tokens=True) for t in outputs]
-            print("======")
-            print("\n===\n".join(text))
-            print("======")
+            print0("======")
+            print0("\n===\n".join(text))
+            print0("======")
 
-        with set_adapter(model, "default"):
-            indices = torch.randint(0, len(prompts), (args.batch_size,), device=device)
-            tokens = model.generate(
-                inputs.input_ids[indices],
-                attention_mask=inputs.attention_mask[indices],
-                do_sample=True,
-                min_new_tokens=args.length,
-                max_new_tokens=args.length,
-                pad_token_id=tokenizer.eos_token_id,
-                top_k=0,
+        with accelerator.accumulate(model):
+            with set_adapter(accelerator.unwrap_model(model), "default"):
+                indices = torch.randint(0, len(prompts), (args.batch_size,), device=device)
+                tokens = accelerator.unwrap_model(model).generate(
+                    inputs.input_ids[indices],
+                    attention_mask=inputs.attention_mask[indices],
+                    do_sample=True,
+                    min_new_tokens=args.length,
+                    max_new_tokens=args.length,
+                    pad_token_id=tokenizer.eos_token_id,
+                    top_k=0,
+                )
+                attention_mask = torch.cat(
+                    [inputs.attention_mask[indices], torch.ones_like(tokens[:, input_len:])], dim=1
+                )
+                outputs = model(tokens, attention_mask=attention_mask)
+            texts = [tokenizer.decode(t, skip_special_tokens=True) for t in tokens]
+            logits = at_least_float32(outputs.logits)
+            logp = dice.logp_categorical(logits[:, input_len - 1 : -1], tokens[:, input_len:])
+            logp_sum = torch.sum(logp, dim=1)
+            logp_cumsum = torch.cumsum(logp, dim=1)
+
+            with torch.no_grad(), set_adapter(accelerator.unwrap_model(model), None):
+                outputs_orig = model(tokens, attention_mask=attention_mask)
+            logits_orig = at_least_float32(outputs_orig.logits)
+            logp_orig = dice.logp_categorical(
+                logits_orig[:, input_len - 1 : -1], tokens[:, input_len:]
             )
-            attention_mask = torch.cat(
-                [inputs.attention_mask[indices], torch.ones_like(tokens[:, input_len:])], dim=1
+            logp_orig_cumsum = torch.cumsum(logp_orig, dim=1)
+            kls = inv_cumsum(kl_div_est(logp_cumsum.detach(), logp_orig_cumsum.detach()))
+
+            split_texts = [
+                {"prompt": prompts[index], "response": text[len(prompts[index]) :]}
+                for text, index in zip(texts, indices)
+            ]
+            eval_prompts = make_prompts_for_scoring(cons, split_texts)
+            scores = []
+            with torch.no_grad(), set_adapter(accelerator.unwrap_model(model), "evaluator"):
+                for eval_prompt_batch in eval_prompts:
+                    if eval_split_batches:
+                        scores.append([])
+                        for eval_prompt in eval_prompt_batch:
+                            eval_inputs = tokenizer(eval_prompt, return_tensors="pt").to(device)
+                            eval_outputs = model(eval_inputs.input_ids)
+                            score = get_scores(eval_outputs.logits)
+                            scores[-1].append(score)
+                        scores[-1] = torch.cat(scores[-1])
+                    else:
+                        eval_inputs = tokenizer(
+                            eval_prompt_batch, return_tensors="pt", padding=True
+                        ).to(device)
+                        eval_outputs = model(
+                            eval_inputs.input_ids, attention_mask=eval_inputs.attention_mask
+                        )
+                        scores.append(get_scores(eval_outputs.logits))
+            scores = torch.stack(scores, dim=1)
+            scores = soft_minimum(
+                scores * principle_signs[None], principle_weights[None], dim=1
+            ).to(device)
+
+            losses_main = -F.logsigmoid(scores)
+            losses_main = dice.cost_node(losses_main, [logp_sum])
+            losses_main_global = accelerator.reduce(losses_main, "mean")
+            losses_main += baseline(losses_main_global, [logp_sum])
+            losses_kl = kls * kl_sched(i)
+            losses_kl = dice.cost_node(losses_kl, [logp_cumsum])
+            losses_kl_global = accelerator.reduce(losses_kl, "mean")
+            losses_kl += baseline_kl(losses_kl_global, [logp_cumsum])
+            loss_main = losses_main.mean()
+            loss_kl = losses_kl.mean()
+            loss = loss_main + loss_kl
+            loss_global = accelerator.reduce(loss, "mean")
+            accelerator.backward(loss)
+
+            print0(
+                f"step: {i}, loss: {loss_global.item():g}, main: {losses_main_global.mean().item():g}, kl: {losses_kl_global.mean().item():g}"
             )
-            outputs = model(tokens, attention_mask=attention_mask)
-        texts = [tokenizer.decode(t, skip_special_tokens=True) for t in tokens]
-        logits = at_least_float32(outputs.logits)
-        logp = dice.logp_categorical(logits[:, input_len - 1 : -1], tokens[:, input_len:])
-        logp_sum = torch.sum(logp, dim=1)
-        logp_cumsum = torch.cumsum(logp, dim=1)
 
-        with torch.no_grad(), set_adapter(model, None):
-            outputs_orig = model(tokens, attention_mask=attention_mask)
-        logits_orig = at_least_float32(outputs_orig.logits)
-        logp_orig = dice.logp_categorical(logits_orig[:, input_len - 1 : -1], tokens[:, input_len:])
-        logp_orig_cumsum = torch.cumsum(logp_orig, dim=1)
-        kls = inv_cumsum(kl_div_est(logp_cumsum.detach(), logp_orig_cumsum.detach()))
+            if accelerator.sync_gradients:
+                grad_norm = 0.0
+                for p in model.parameters():
+                    if p.grad is not None:
+                        grad_norm += p.grad.pow(2).sum().item()
+                grad_norm **= 0.5
+                print0(f"grad norm: {grad_norm:g}")
 
-        split_texts = [
-            {"prompt": prompts[index], "response": text[len(prompts[index]) :]}
-            for text, index in zip(texts, indices)
-        ]
-        eval_prompts = make_prompts_for_scoring(cons, split_texts)
-        scores = []
-        with torch.no_grad(), set_adapter(model, "evaluator"):
-            for eval_prompt_batch in eval_prompts:
-                if eval_split_batches:
-                    scores.append([])
-                    for eval_prompt in eval_prompt_batch:
-                        eval_inputs = tokenizer(eval_prompt, return_tensors="pt").to(device)
-                        eval_outputs = model(eval_inputs.input_ids)
-                        score = get_scores(eval_outputs.logits)
-                        scores[-1].append(score)
-                    scores[-1] = torch.cat(scores[-1])
-                else:
-                    eval_inputs = tokenizer(
-                        eval_prompt_batch, return_tensors="pt", padding=True
-                    ).to(device)
-                    eval_outputs = model(
-                        eval_inputs.input_ids, attention_mask=eval_inputs.attention_mask
-                    )
-                    scores.append(get_scores(eval_outputs.logits))
-        scores = torch.stack(scores, dim=1)
-        scores = soft_minimum(scores, principle_weights[None], dim=1).to(logp.device)
-
-        losses_main = -F.logsigmoid(scores)
-        losses_main = dice.cost_node(losses_main, [logp_sum])
-        losses_main += baseline(losses_main, [logp_sum])
-        losses_kl = kls * kl_sched(i)
-        losses_kl = dice.cost_node(losses_kl, [logp_cumsum])
-        losses_kl += baseline_kl(losses_kl, [logp_cumsum])
-        loss_main = losses_main.mean()
-        loss_kl = losses_kl.mean()
-        loss = loss_main + loss_kl
-        scaler.scale(loss / args.grad_accum_steps).backward()
-        print(
-            f"step: {i}, loss: {loss.item():g}, main: {loss_main.item():g}, kl: {loss_kl.item():g}"
-        )
-
-        if (i + 1) % args.grad_accum_steps == 0:
-            scaler.unscale_(opt)
-            grad_norm = 0.0
-            for p in model.parameters():
-                if p.grad is not None:
-                    grad_norm += p.grad.pow(2).sum().item()
-            grad_norm **= 0.5
-            print(f"step: {i}, grad_norm: {grad_norm:g}")
-            scaler.step(opt)
-            scaler.update()
+            opt.step()
             sched.step()
             opt.zero_grad()
 
-        if i > 0 and i % args.save_every == 0:
+        if accelerator.is_main_process and i > 0 and i % args.save_every == 0:
             print("Saving model...", file=sys.stderr)
             tokenizer.save_pretrained(args.output_path)
-            model.save_pretrained(
+            accelerator.unwrap_model(model).save_pretrained(
                 args.output_path, safe_serialization=True, selected_adapters=["default"]
             )
 
