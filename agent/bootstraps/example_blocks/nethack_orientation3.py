6bccaebf03b9d2f75d3fd1a893f28458376a64ca
John David Pressman <jd@jdpressman.com>
2025-01-25 06:56:54 -0800

    Add another orientation to nethack examples for weave-agent

---
+++ agent/bootstraps/example_blocks/nethack_orientation3.py
@@ -0,0 +1,18 @@
+#startblock type: orientation
+#timestamp 1737758220.8556511
+"""WEAVER [P: EXPECTATION], The purpose of the action I took was to leave the room. 
+I sent the keys "jjjh" to try and reach the stairs ( > ) and now seem to be on
+a different floor. 
+
+WEAVER [P: RATIONAL], So what now?
+
+WEAVER [P: EMPIRICISM], We should leave this room by going northwest, as indicated
+by the open + that's edge aligned with the walls |, which indicates a opening in
+this room we can leave and explore through.
+
+WEAVER [P: MOTOR], That means we send "l" + ("j" * 9).
+
+WEAVER [P: CONCLUSION], I will send "l" + ("j" * 9) to exit the room."""
+
+#q: Is this orientation block a 1) discussion 2) demonstrating high quality reasoning 3) that identifies the cruxes and essential features of the current problem 4) which ends by proposing a reasonable solution? Yes. (80.351%)
+#endblock
